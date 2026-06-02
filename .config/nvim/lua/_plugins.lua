7a5943bc6cf57b702fd7294e1f01da08d50ed61a
Alex Shi <hlcfan.yan@gmail.com>
2026-06-02 14:18:56 +0800

    Configure hlslens key mappings
    Added key mappings for hlslens functionality in Neovim.

--- .config/nvim/lua/_plugins.lua
+++ .config/nvim/lua/_plugins.lua
@@ -497,7 +497,7 @@ require("lazy").setup({
       -- log_level = 'debug',
     }
   },
-  'subnut/nvim-ghost.nvim',
+  -- 'subnut/nvim-ghost.nvim',
   {
     "NeogitOrg/neogit",
     dependencies = {
@@ -688,3 +688,18 @@ require("catppuccin").setup({
 })
 
 require('hlslens').setup()
+
+local kopts = {noremap = true, silent = true}
+
+vim.api.nvim_set_keymap('n', 'n',
+    [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+    kopts)
+vim.api.nvim_set_keymap('n', 'N',
+    [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+    kopts)
+vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+
+vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>noh<CR>', kopts)
